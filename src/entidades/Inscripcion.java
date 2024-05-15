f5074a468133a2362d69e3c1d219189b9cd8c314
FerrandoCarlos <cferrando2015@gmail.com>
2024-05-15 19:36:46 -0300

    cambios en entidad Inscripción

--- src/entidades/Inscripcion.java
+++ src/entidades/Inscripcion.java
@@ -1,10 +1,66 @@
 
 package entidades;
-
 /**
- *
+ *@author Ferrando Carlos
  * @author Martin
  */
 public class Inscripcion {
+    private int inscripcion;
+    private Alumno alumno;
+    private Materia materia;
+    private double nota;
+
+    public Inscripcion(int inscripcion, Alumno alumno, Materia materia, double nota) {
+        this.inscripcion = inscripcion;
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+    }
+
+    public Inscripcion(Alumno alumno, Materia materia, double nota) {
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+    }
+
+    public Inscripcion() {
+    }
+
+    public Inscripcion(double nota) {
+        this.nota = nota;
+    }
+
+    public int getInscripcion() {
+        return inscripcion;
+    }
+
+    public void setInscripcion(int inscripcion) {
+        this.inscripcion = inscripcion;
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+    
     
 }
