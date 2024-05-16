c913790660fc949d507cb8e5294298817178bb96
aguz11 <agus110196@gmail.com>
2024-05-16 12:35:43 -0300

    Update InscripcionData.java

--- src/accesoADatos/InscripcionData.java
+++ src/accesoADatos/InscripcionData.java
@@ -5,13 +5,6 @@ import entidades.Materia;
 import java.sql.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
-
-<<<<<<< Updated upstream
-import entidades.Inscripcion;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import javax.swing.JOptionPane;
 
 /**
@@ -23,33 +16,7 @@ public class InscripcionData {
      * Guarda una inscripción en la base de datos.
      * @param inscripcion La inscripción a guardar.
      * @return true si se guardó correctamente, false si ocurrió un error.
-     */
-    
-    public static boolean guardarInscripcion(Inscripcion inscripcion){
-        String query = "INSERT INTO inscripcion (idAlumno,idMateria,nota) VALUES (?,?,?)";  
-        boolean res = false;
-        if (Db.getConexion()) {
-            try {
-                PreparedStatement ps = Db.conec.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-              
-                ps.setInt(1, inscripcion.getAlumno().getIdAlumno());
-                ps.setInt(2, inscripcion.getMateria().getIdMateria());
-                ps.setDouble(3, inscripcion.getNota());
-           
-                ps.executeUpdate();
-                ResultSet rs = ps.getGeneratedKeys();
-                if (rs.next()) {
-                  inscripcion.setIdInscripcion(rs.getInt(1));
-                    res = true;
-                }
-                ps.close();
-            } catch (SQLException ex) {
-                Db.msjError.add("Inscripción: guardarInscripcion ->" + ex.getMessage());
-            }
-        }
-        return res; 
-    }
-    
+     */   
     public static boolean actulizarNota(int idAlumno,int idMateria, double nota){
         String query = "UPDATE inscripcion SET nota = ? WHERE idAlumno = ? and idMateria = ?";
         
@@ -74,14 +41,6 @@ public class InscripcionData {
             }
         }
         return res;
-    }
-=======
-public class InscripcionData {
-    private Connection cc = null;
-    
-    
-    public InscripcionData(){
-
     }
     
     
@@ -130,5 +89,4 @@ public class InscripcionData {
     
     return exito;
 }
->>>>>>> Stashed changes
 }
